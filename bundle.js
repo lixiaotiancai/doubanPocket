c746cbd532da5a717e4a2e5ca81223c58801b62a
lixiaotiancai <1422975258@qq.com>
2018-06-02 21:25:22 +0800

    口袋豆瓣项目更改

--- bundle.js
+++ bundle.js
@@ -21219,6 +21219,8 @@ var SearchPage = function (_Component) {
       var touch = e.touches[0];
       var startY = touch.pageY;
 
+      e.preventDefault();
+
       this.setState({
         startY: startY
       });
